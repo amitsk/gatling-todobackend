ad2513518cf43389c562c2a78b91c4bf29a0e19c
amitsk <amitsk1974@gmail.com>
2016-07-01 23:14:43 -0700

    Delete, Patch added. Readme added

---
+++ README.md
@@ -0,0 +1,5 @@
+Example of a Gatling test for a microservice.
+
+This test is written against this implementation : https://github.com/jcsantosbr/todo-backend-spring4-java8
+
+To run the tests run the maven command `mvn clean gatling:test`
--- src/test/scala/com/github/amitsk/todobackend/TodoSimulation.scala
+++ src/test/scala/com/github/amitsk/todobackend/TodoSimulation.scala
@@ -1,27 +1,30 @@
 package com.github.amitsk.todobackend
 
-import com.github.amitsk.todobackend.Operations.{createTodo, getTodo}
+import com.github.amitsk.todobackend.Operations._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.http.check.HttpCheck
-
 import scala.concurrent.duration._
+
 
 class TodoSimulation extends Simulation {
 
   val httpConf = http
     .baseURL("http://localhost:8080/todos") // Here is the root for all relative URLs
-    //.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
-    //.acceptEncodingHeader("gzip, deflate")
-    //.acceptLanguageHeader("en-US,en;q=0.5")
-
-  val checks = status.is(200)
+    .acceptEncodingHeader("gzip, deflate")
 
   val scn = scenario("Create TODOs") // A scenario is a chain of requests and pauses
-    .exec( createTodo())
-    .exec {
-      getTodo(checks)
-    }
+    .exec(createTodo())
+    .exec(getTodo())
+    .exec(updateTodo())
+    .exec(deleteTodo())
 
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
+  setUp(scn.
+    inject(
+      nothingFor(1 seconds),
+      atOnceUsers(2),
+      rampUsers(1) over(2 seconds),
+      constantUsersPerSec(2) during(2 seconds) randomized
+    )
+
+    .protocols(httpConf))
 }
